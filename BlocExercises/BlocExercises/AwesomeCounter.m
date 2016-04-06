31f45d30f6b476c4b479c8644831697f87ba17b1
Eddy Chan <echan162@gmail.com>
2016-04-05 17:15:51 -0700

    Assignment 12 loops: completed

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,22 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
+    
+    // Reverse the order, if number is larger than otherNumber
+    // So, this function always returns a string with numbers in ascending order
+    if (number > otherNumber) {
+        NSInteger temp = number;
+        number = otherNumber;
+        otherNumber = temp;
+    }
+    
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    
+    for (NSInteger i = number; i <= otherNumber; i++) {
+        [result addObject:[NSNumber numberWithInteger:i]];
+    }
+    
+    return [result componentsJoinedByString:@""];
 }
 
 @end
