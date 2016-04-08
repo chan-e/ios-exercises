7906979280579418e725a1f2adc2b20cb7cc0d3d
Eddy Chan <echan162@gmail.com>
2016-04-08 16:04:54 -0700

    Assignment 15 data types and objects: completed

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,34 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    
+    // Assuming number holds only an integer
+    return @([number integerValue] * 2);
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    
+    // Reverse the order, if number is larger than otherNumber
+    // So, this function always returns an array of numbers in ascending order
+    if (number > otherNumber) {
+        NSInteger temp = number;
+        number = otherNumber;
+        otherNumber = temp;
+    }
+    
+    NSMutableArray *numbers = [NSMutableArray arrayWithCapacity:(otherNumber - number + 1)];
+    for (NSInteger i = number; i <= otherNumber; i++) {
+        [numbers addObject:[NSNumber numberWithInteger:i]];
+    }
+    
+    return [NSArray arrayWithArray:numbers];
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    
+    return [[arrayOfNumbers valueForKeyPath:@"@min.self"] integerValue];
 }
 
 @end
